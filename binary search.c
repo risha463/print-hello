14ff6781c984ce0d340d1908638ff171c54d0dcb
risha <101916496+risha463@users.noreply.github.com>
2022-06-23 19:04:36 +0530

    Add files via upload

---
+++ binary search.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+    printf("swati\n");
+    int a[20],i,n,loc,item;
+    int bsearch(int[],int,int);
+    printf("enter number of sorted elements(<=20)=");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        printf("enter element a[%d]--",i);
+        scanf("%d",&a[i]);
+    }
+    printf("enter item to search=");
+    scanf("%d",&item);
+    loc=bsearch(a,n,item);
+    if(loc== -1)
+    printf("item does not exist in array");
+    else
+    printf("item\t%d\t exist at index=%d",item, loc);
+    return 0;
+}
+int bsearch(int a[],int n,int item)
+{
+    int loc= -1,beg=0, end=n-1,mid;
+    while(beg<=end)
+    {
+        mid=(beg+end)/2;
+        if(item==a[mid])
+        {
+            loc=mid;
+            break;
+        }
+        else if(item>a[mid])
+        beg=mid+1;
+        else
+        end=mid-1;
+    }
+    return(loc);
+}
---
+++ bubble sorting.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+    printf("swati\n");
+    int a[20],n,i;
+    int bubblesort(int[],int);
+    printf("enter number of elements(<=20)=");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        printf("enter element a[%d]=",i);
+        scanf("%d",&a[i]);
+    }
+    bubblesort(a,n);
+    printf("sorted elements---");
+    for(i=0;i<n;i++)
+    printf("%d \t",a[i]);
+    return 0;
+}
+int bubblesort(int a[],int n)
+{
+    int temp,i,j;
+    for(i=1;i<=n-1;i++)
+    {
+        for(j=0;j<=n-i-1;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+}
